15104f3445e4750a95c7c7a256f3462fa98179e1
Minh Dang <minhmike200@gmail.com>
2022-11-21 18:01:04 -0800

    added Ahmar's algorithm finished

--- Algorithm.cpp
+++ Algorithm.cpp
@@ -9,6 +9,37 @@ Algorithm::Algorithm(Data mat, Data por) {
 
 void Algorithm::printObeservationsAhmar()
 {
+	std::cout << "-------Math Course Weekday Alcohol Consumption-------" << std::endl;
+	//call printAverages() on line#16 with 4 parameters: 
+	//1. Math Students Daily Alcohol Consumption vector
+	//2. frequencyAbsent() function with parameters: Math Students Daily Alcohol Consumption vector, Absences vector
+	//3. integer 0 which corresponds to weekday
+	//4. integer 0 which corresponds to math course
+	printAverages(frequency(mat.getDAlc(), 5, 1), frequencyAbsent(mat.getDAlc(), mat.getAbsences()), 0, 0);
+	std::cout << std::endl;
+	std::cout << "-------Math Course Weekend Alcohol Consumption-------" << std::endl;
+	//call printAverages() on line#24 with 4 parameters: 
+	//1. Math Students Weekend Alcohol Consumption vector
+	//2. frequencyAbsent() function with parameters: Math Students Weekend Alcohol Consumption vector, Absences vector
+	//3. integer 0 which corresponds to weekdend
+	//4. integer 0 which corresponds to math course
+	printAverages(frequency(mat.getWAlc(), 5 ,1), frequencyAbsent(mat.getWAlc(), mat.getAbsences()), 1, 0);
+	std::cout << std::endl;
+	std::cout << "-------Portuguese Course Weekday Alcohol Consumption-------" << std::endl;
+	//call printAverages() on line#32 with 4 parameters: 
+	//1. Portuguese Students Daily Alcohol Consumption vector
+	//2. frequencyAbsent() function with parameters: Portuguese Students Daily Alcohol Consumption vector, Absences vector
+	//3. integer 0 which corresponds to weekday
+	//4. integer 0 which corresponds to portuguese course
+	printAverages(frequency(por.getDAlc(), 5, 1), frequencyAbsent(por.getDAlc(), por.getAbsences()), 0, 1);
+	std::cout << std::endl;
+	std::cout << "-------Portuguese Course Weekend Alcohol Consumption-------" << std::endl;
+	//call printAverages() on line#40 with 4 parameters: 
+	//1. Portuguese Students Weekend Alcohol Consumption vector
+	//2. frequencyAbsent() function with parameters: Portuguese Students Weekend Alcohol Consumption vector, Absences vector
+	//3. integer 0 which corresponds to weekdend
+	//4. integer 0 which corresponds to portuguese course
+	printAverages(frequency(por.getWAlc(), 5, 1), frequencyAbsent(por.getWAlc(), por.getAbsences()), 1, 1);
 	//Print Observations
 	std::cout << std::endl;
 	std::cout << "-------Observations--------" << std::endl;
